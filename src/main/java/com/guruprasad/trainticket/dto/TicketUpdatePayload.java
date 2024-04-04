4ef0650c52ef089434cacee55b359dcb10d81915
gbhat <bpg168g@gmail.com>
2024-04-04 11:58:17 +0530

    add an all arg constructor for the TicketUpdatePayload

--- src/main/java/com/guruprasad/trainticket/dto/TicketUpdatePayload.java
+++ src/main/java/com/guruprasad/trainticket/dto/TicketUpdatePayload.java
@@ -1,8 +1,10 @@
 package com.guruprasad.trainticket.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class TicketUpdatePayload {
     String newSection;
     Integer newSeat;
