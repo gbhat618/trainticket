8662d0b0376be030c771ecaf4fadc664d0ecd1a9
gbhat <bpg168g@gmail.com>
2024-04-06 16:43:38 +0530

    Use the liquibase field setting available in springboot tests as well.
    Speed previous 10+sec to 440ms

--- src/main/java/com/guruprasad/trainticket/TrainticketApplication.java
+++ src/main/java/com/guruprasad/trainticket/TrainticketApplication.java
@@ -1,24 +1,14 @@
 package com.guruprasad.trainticket;
 
-import liquibase.util.NetUtil;
+import com.guruprasad.trainticket.snippets.StaticCodeSnippets;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.lang.reflect.Field;
 
 @SpringBootApplication
 public class TrainticketApplication {
 
-	private static void setLiquibaseNetUtilsLocalHost() throws NoSuchFieldException, IllegalAccessException {
-		Field field = NetUtil.class.getDeclaredField("hostName");
-		field.setAccessible(true);
-		field.set(null, "localhost");
-		field.setAccessible(false);
-	}
-
-	public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
-		setLiquibaseNetUtilsLocalHost();
-
+	public static void main(String[] args) {
+		StaticCodeSnippets.setLiquibaseNetUtilsLocalHost();
 		SpringApplication.run(TrainticketApplication.class, args);
 	}
 
