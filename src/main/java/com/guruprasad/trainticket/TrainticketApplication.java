f93af7454c3cf11ffa677f78080d171bab159785
gbhat <bpg168g@gmail.com>
2024-04-06 15:01:05 +0530

    Workaroud #2 - Temporary patch for speeding up liquibase getLocalhost during boot up
    Liquibase utils getLocalhostName as taking 10s. It is reported in a few github issues. Unfortunately at the moment it is only patched for the Mac books.
    
    Currently working around it for ubuntu like.

--- src/main/java/com/guruprasad/trainticket/TrainticketApplication.java
+++ src/main/java/com/guruprasad/trainticket/TrainticketApplication.java
@@ -1,12 +1,24 @@
 package com.guruprasad.trainticket;
 
+import liquibase.util.NetUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.lang.reflect.Field;
 
 @SpringBootApplication
 public class TrainticketApplication {
 
-	public static void main(String[] args) {
+	private static void setLiquibaseNetUtilsLocalHost() throws NoSuchFieldException, IllegalAccessException {
+		Field field = NetUtil.class.getDeclaredField("hostName");
+		field.setAccessible(true);
+		field.set(null, "localhost");
+		field.setAccessible(false);
+	}
+
+	public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+		setLiquibaseNetUtilsLocalHost();
+
 		SpringApplication.run(TrainticketApplication.class, args);
 	}
 
